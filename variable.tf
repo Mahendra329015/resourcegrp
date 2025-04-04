ee838ee2a9feaf91b9423e4c46bdad37ffa63f3f
Mahendra329015 <gutthamahendra329@gmail.com>
2025-04-04 13:14:15 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,3 +3,24 @@ variable "Resource_group_name" {
   #default = "user"
   default = "default_value"
 }
+# Service Principal Variables
+
+variable "client_id" {
+    description =   "Client ID (APP ID) of the application"
+    type        =   string
+}
+
+variable "client_secret" {
+    description =   "Client Secret (Password) of the application"
+    type        =   string
+}
+
+variable "subscription_id" {
+    description =   "Subscription ID"
+    type        =   string
+}
+
+variable "tenant_id" {
+    description =   "Tenant ID"
+    type        =   string
+}
